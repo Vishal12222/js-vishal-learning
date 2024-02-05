95afb467e3e255e6a7b7eabbfefd69bebcde11de
Vishal12222 <101244765+Vishal12222@users.noreply.github.com>
2024-02-05 11:11:34 +0000

    Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1); //true
+// console.log(2 >= 1); //true
+// console.log(2 < 1);  //false
+// console.log(2 == 1); //false
+// console.log(2 != 1); //true
+
+//console.log("2" > 1); //true
+//console.log("02" > 1); //true
+
+// console.log(null > 0); //false    We Avoid this type of Code
+// console.log(null == 0);  //false
+// console.log(null >= 0); //true
+
+console.log(undefined == 0); //false    We Avoid this type of Code
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+
+// === (not check values only but check datatype of values also)
+// console.log("2" === 2); //false
