068186d8b8c1c834e74c03317825c5bfd949fae6
Vishal12222 <101244765+Vishal12222@users.noreply.github.com>
2024-02-06 15:30:00 +0000

    objects_json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -30,7 +30,7 @@ const obj3 = {5: "e",  6:"f"}
 const obj4 = Object.assign({}, obj1, obj2 , obj3) // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
 
 const obj5 = {...obj1, ...obj2, ...obj3} // this is called spread operartor
-console.log(obj5); // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+//console.log(obj5); // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
 
 //if value comes from database then we use this
 
@@ -50,12 +50,40 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));  // [ 'id', 'name', 'isLoggedIn' ]
-console.log(Object.values(tinderUser));  // [ '1234abc', 'Vishal', false ]
-console.log(Object.entries(tinderUser)); // [ [ 'id', '1234abc' ], [ 'name', 'Vishal' ], [ 'isLoggedIn', false ] ]
+// console.log(Object.keys(tinderUser));  // [ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser));  // [ '1234abc', 'Vishal', false ]
+// console.log(Object.entries(tinderUser)); // [ [ 'id', '1234abc' ], [ 'name', 'Vishal' ], [ 'isLoggedIn', false ] ]
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // true (hasOwnProperty checks property exist or not)
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // true (hasOwnProperty checks property exist or not)
 
-console.log(tinderUser.hasOwnProperty('isLogged'));  // false  (hasOwnProperty checks property exist or not)
+// console.log(tinderUser.hasOwnProperty('isLogged'));  // false  (hasOwnProperty checks property exist or not)
+
+const course ={
+    coursename : "JS",
+    price : "999",
+    courseInstructor : "Vishal"
+}
+
+// course.courseInstructor
+
+// const {courseInstructor} = course (if you think courseInstructor is big name then we use | courseInstructor: instructor) // Vishal
+const {courseInstructor: instructor} = course // Vishal
+
+console.log(instructor); // Vishal
+
+
+//Json API
+
+// {
+//     "name":"vishal",
+//     "coursename":"js",
+//     "price":0
+// }
+
+[
+    {},
+    {},
+    {}
+]
